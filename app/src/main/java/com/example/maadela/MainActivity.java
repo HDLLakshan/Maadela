b9b4c28748fb7ef2d5f9e019109a807af2e84105
HDLLakshan <HDLLakshan@users.noreply.github.com>
2019-08-05 11:33:34 +0530

    Initial version with my function

---
+++ app/src/main/java/com/example/maadela/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.maadela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        setContentView( R.layout.activity_main );
+
+        Handler handler = new Handler(  );
+        handler.postDelayed( new Runnable() {
+            @Override
+            public void run() {
+               Intent intent = new Intent( MainActivity.this, SearchNavi.class );
+               startActivity(intent);
+                finish();
+            }
+        },2500 );
+    }
+}
---
+++ app/src/main/java/com/example/maadela/SearchNavi.java
@@ -0,0 +1,128 @@
+package com.example.maadela;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SearchNavi extends Activity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Button btn;
+       private static final String[] Fish = new String[]{"Balaya","Thora","Thalmaha","Tuuna"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        setContentView( R.layout.activity_search_navi );
+        btn= (Button)findViewById( R.id.search );
+        AutoCompleteTextView et = findViewById( R.id.fishname );
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( this,android.R.layout.simple_list_item_1,Fish );
+        et.setAdapter( adapter );
+        Toolbar toolbar = findViewById( R.id.toolbar );
+       //  setSupportActionBar( toolbar );
+        FloatingActionButton fab = findViewById( R.id.fab );
+        fab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
+                        .setAction( "Action", null ).show();
+            }
+        } );
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        NavigationView navigationView = findViewById( R.id.nav_view );
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close );
+        drawer.addDrawerListener( toggle );
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener( this );
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        if (drawer.isDrawerOpen( GravityCompat.START )) {
+            drawer.closeDrawer( GravityCompat.START );
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.search_navi, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected( item );
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        drawer.closeDrawer( GravityCompat.START );
+        return true;
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+       // EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+     //   intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
