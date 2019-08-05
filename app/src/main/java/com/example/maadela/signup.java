7ad50373aeb4a25e8fbcf8859165cb00c7e61f2e
HDLLakshan <HDLLakshan@users.noreply.github.com>
2019-08-05 14:44:38 +0530

    Initial version with my function

--- app/src/main/java/com/example/maadela/signup.java
+++ app/src/main/java/com/example/maadela/signup.java
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class signup extends AppCompatActivity {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
+       btn = (Button)findViewById( R.id.button );
     }
 
-    public void sendMessage(View view) {
+    public void sendMessage1(View view) {
         Intent intent = new Intent(this, SearchNavi.class);
         // EditText editText = (EditText) findViewById(R.id.editText);
         // String message = editText.getText().toString();
